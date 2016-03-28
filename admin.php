99525fd1dcd65902f4ffda1e5231ad92c7032750
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2016-03-27 21:02:28 -0400

    added admin page

---
+++ admin.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<link rel="stylesheet" href="stylesheets/admin.css">
+<title>Teacher Dashboard</title>
+</head>
+
+<body>
+
+<p class="header" align="left">
+    UCP Bailes
+</p>
+<p class="header_right" align="right">
+    <br><br><br>Hello, Instructor <br>
+    Sign Out
+    <hr>
+</p>
+<div class="left_recs">
+    <tr>
+        <td>Classes</td>
+        <td>WE</td>
+    </tr>
+</div>
+
+
+
+</body>
+
+</html>
